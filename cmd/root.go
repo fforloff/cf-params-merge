940901bd4607f2e52c25d77f3497a0b547df2f3f
Victor <victor.orlov@irexchange.com>
2018-02-12 21:15:28 +1100

    added mmissing dependencies

---
+++ cmd/root.go
@@ -0,0 +1,74 @@
+// Copyright © 2018 NAME HERE <EMAIL ADDRESS>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cmd
+
+import (
+	"fmt"
+	"os"
+	"errors"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/pflag"
+)
+
+
+
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "cfmingle",
+	Short: "Hierarchically merge AWS CloudFormation parameters",
+	Long: `cfmingle is a CLI tool that takes AWS CloudFormation templates
+parameters from different sources - the template itself, external files,
+environment variables and merge them into a single parameters JSON`,
+	PreRunE: func(cmd *cobra.Command, args []string) error {
+		return CheckRequiredFlags(cmd.Flags())
+	},
+}
+
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func init() {
+}
+
+func CheckRequiredFlags(flags *pflag.FlagSet) error {
+	requiredError := false
+	flagName := ""
+
+	flags.VisitAll(func(flag *pflag.Flag) {
+		requiredAnnotation := flag.Annotations[cobra.BashCompOneRequiredFlag]
+		if len(requiredAnnotation) == 0 {
+			return
+		}
+
+		flagRequired := requiredAnnotation[0] == "true"
+
+		if flagRequired && !flag.Changed {
+			requiredError = true
+			flagName = flag.Name
+		}
+	})
+
+	if requiredError {
+		return errors.New("Required flag `" + flagName + "` has not been set")
+	}
+
+	return nil
+}
